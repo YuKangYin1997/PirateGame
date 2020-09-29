6e405669ebe663a78d4f2280ecd7c52d5c93a1bd
YuKangYin <yukangyin@outlook.com>
2020-09-29 16:42:14 +0800

    [TEST] Row 75 Test Fail

--- src/test/java/com/a1/test/Part1Test.java
+++ src/test/java/com/a1/test/Part1Test.java
@@ -1,5 +1,6 @@
 package com.a1.test;
 
+import com.a1.game.Game;
 import com.a1.game.GameMode;
 import com.a1.player.Player;
 import com.a1.util.Card;
@@ -24,5 +25,17 @@ public class Part1Test {
         int score = player.playTurn(dieRoll, card, new Scanner(in));
         Assert.assertTrue(player.playerIsDead(dieRoll, card));
         Assert.assertEquals(0, score);
+    }
+
+    @Test
+    public void reRollIndexValidTest75() {
+        // get 7 swords on first roll, try to roll the 8 die by itself -> interface reports not allowed
+        GameMode.mode = GameMode.JUNIT_TEST;
+        String[] dieRoll = {Const.DICE_SWORD, Const.DICE_SWORD, Const.DICE_SWORD, Const.DICE_SWORD,
+                Const.DICE_SWORD, Const.DICE_SWORD, Const.DICE_SWORD, Const.DICE_MONKEY};
+        Card card = new Card(Const.CARD_COIN);
+        int[] reRollIndexes = {7};
+        boolean reRollAllowed = Game.isReRollIndexValid(dieRoll, reRollIndexes, card);
+        Assert.assertFalse(reRollAllowed);
     }
 }
