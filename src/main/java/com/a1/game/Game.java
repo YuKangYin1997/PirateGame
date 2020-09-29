a71022a65b9fd12c4f6455ac481b885382d80c3d
YuKangYin <yukangyin@outlook.com>
2020-09-29 16:44:07 +0800

    [CODE] Pass Row 75 Test

--- src/main/java/com/a1/game/Game.java
+++ src/main/java/com/a1/game/Game.java
@@ -3,6 +3,8 @@ package com.a1.game;
 import com.a1.util.Card;
 import com.a1.util.Const;
 
+import java.util.List;
+
 public class Game {
 
     /**
@@ -54,5 +56,41 @@ public class Game {
         System.out.print("Current Card : ");
         System.out.print("[");
         System.out.println(card.getName().substring(card.getName().indexOf("_") + 1) + "]");
+    }
+
+    /**
+     * if a player accumulate less than 3 skulls and choose to re-roll, considering
+     * 1. at least two dice are re-rolled
+     * 2. if player has a treasure chest card, the dice in the chest shouldn't be re-rolled
+     * 3. any skull can't be re-rolled
+     * 4. boundary check
+     * checks if re-roll indexes are valid
+     */
+    public static boolean isReRollIndexValid(String[] dieRoll, int[] reRollIndexes, Card card) {
+        if (reRollIndexes.length < 2) {
+            System.out.println("Sorry, re-roll in not allowed...");
+            return false;
+        }
+
+        if (card.getName().equals(Const.CARD_TREASURE_CHEST)) {
+            List<Integer> treasureChest = card.getTreasureChest();
+            if (treasureChest != null) {
+                for (int index : reRollIndexes) {
+                    if (treasureChest.contains(index)) {
+                        System.out.println("Sorry, re-roll in not allowed...");
+                        return false;
+                    }
+                }
+            }
+        }
+
+        // make sure skull shouldn't be re-rolled
+        for (int index : reRollIndexes) {
+            if (index > 7 || dieRoll[index].equals(Const.DICE_SKULL)) {
+                System.out.println("Sorry, re-roll in not allowed...");
+                return false;
+            }
+        }
+        return true;
     }
 }
